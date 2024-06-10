6fbddc709ba501cf380ff90678c9793b8cef315d
Kirill Suvorov <kirillsuvorov@MacBook-Air-Kirill.local>
2024-06-11 03:34:45 +0800

    Added robots.txt

--- website/website/urls.py
+++ website/website/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', include('ad.urls', namespace='ad')),
     path('stats/', include('ad_statistics.urls', namespace='ad_statistics')),
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'pages.views.page_not_found'
