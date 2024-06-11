5f18be662a099234380789080c16bfcba05024b0
Kirill Suvorov <kirillsuvorov@MacBook-Air-Kirill.local>
2024-06-11 20:37:22 +0800

    Fixed studio card heigh

--- website/website/settings.py
+++ website/website/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
  #   'default': {
   #      'ENGINE': 'django.db.backends.sqlite3',
    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   # }
+    #}
 #}
 
 AUTH_PASSWORD_VALIDATORS = [
